e6abd93657bec14582bd9bada7688ef5cfc57311
DESKTOP-HJMPDDJ\shanig123 <shanig123@naver.com>
2022-02-21 21:29:57 +0900

    트레일 이펙트 불렛에 붙어서 활성화 되는 것을 확인함 불렛 사망후 트레일 지속되도록 수정이 필요함.

--- Moba3dTowerDF/Assets/Scripts/Effect/GlowTrail_Effect.cs
+++ Moba3dTowerDF/Assets/Scripts/Effect/GlowTrail_Effect.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GlowTrail_Effect : MonoBehaviour
 {
     [SerializeField] private TrailRenderer m_TrailRender;
+    //[SerializeField] private Material m_TrailRenderMaterial;
     [SerializeField] private ParticleSystem m_TrailParticle;
     [SerializeField] private ParticleSystem m_GlowParticle;
     [SerializeField] private ParticleSystem m_DustParticle;
@@ -30,7 +31,9 @@ public class GlowTrail_Effect : MonoBehaviour
 
     private void InitParticleColorInfo()
     {
-        if (m_EffectColor == null)
+        if (m_EffectColor.r == 0 &&
+            m_EffectColor.g == 0 &&
+            m_EffectColor.b == 0)
         {
             m_EffectColor = m_DefaultColor;
         }
@@ -49,29 +52,68 @@ public class GlowTrail_Effect : MonoBehaviour
 
     private void Copy_Info_inParticlesys()
     {
-        //detailcolor가 없다면 false 있으면 true 
+        //effect가 없다면 true 있으면 false 
         //false라면 effectColor값 사용 true라면 디테일 컬러값을 사용한다.
-        bool bCheckDetailColor = true;
-        if (m_DetailColor[0] == null &&
-                m_DetailColor[1] == null &&
-                m_DetailColor[2] == null)
-            bCheckDetailColor = false;
+        bool bCheckDetailColor = false;
+        if (m_EffectColor.r == 0 &&
+            m_EffectColor.g == 0 &&
+            m_EffectColor.b == 0)
+            bCheckDetailColor = true;
 
         if (m_TrailRender != null)
         {
-
+            if(bCheckDetailColor)
+            {
+                //m_TrailRender.colorGradient *= m_DetailColor[0];
+                m_TrailRender.material.color = m_DetailColor[0];
+            }
+            else
+            {
+                m_TrailRender.material.color = m_EffectColor;
+            }
         }
         if(m_TrailParticle != null)
         {
-
+            if (bCheckDetailColor)
+            {
+                //m_TrailRender.colorGradient *= m_DetailColor[0];
+                var ParticleMain = m_TrailParticle.main;
+                ParticleMain.startColor = m_DetailColor[0];
+                //m_TrailParticle.main = ParticleMain;
+                //m_TrailParticle.main = ParticleMain;
+            }
+            else
+            {
+                var ParticleMain = m_TrailParticle.main;
+                ParticleMain.startColor = m_EffectColor;
+            }
         }
         if (m_GlowParticle != null)
         {
-
+            if (bCheckDetailColor)
+            {
+                //m_TrailRender.colorGradient *= m_DetailColor[0];
+                var ParticleMain = m_GlowParticle.main;
+                ParticleMain.startColor = m_DetailColor[1];
+            }
+            else
+            {
+                var ParticleMain = m_GlowParticle.main;
+                ParticleMain.startColor = m_EffectColor;
+            }
         }
         if (m_DustParticle != null)
         {
-
+            if (bCheckDetailColor)
+            {
+                var ParticleMain = m_DustParticle.main;
+                ParticleMain.startColor = m_DetailColor[2];
+            }
+            else
+            {
+                var ParticleMain = m_DustParticle.main;
+                ParticleMain.startColor = m_EffectColor;
+            }
         }
     }
 }
